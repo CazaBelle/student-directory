482705715b60308506fe4fd89c0c1ff6c5eedcc9
Carol Gardiner <carolan.gardiner@gmail.com>
2018-11-18 15:12:23 +0000

    Put code into methods

--- directory.rb
+++ directory.rb
@@ -1,14 +1,28 @@
 
-"The Students of Villains Academy"
-puts "-------------"
+
 
 student_arr = [ "Dr. Hannibal Lecter", "Darth Vader", "NUrse Ratched",
   "Michael Corleone", "Alex DeLarge", "The Wicked Witch of the West",
   "Terminator", "Freddy Krueger", "The Joker", "Joffrey Baratheon",
   "Norman Bates" ]
 
-student_arr.map {|student| puts student }
 
-# print the total number of Students
-puts "Overall, we have #{student_arr.count} great students"
-# important to remember that print() doesn't add new line chars
+# print header
+def print_header
+  puts "The Students of Villa&&&ins Academy"
+  puts "-------------"
+end
+
+#print student list
+def print(names)
+  names.each {|student| puts student }
+end
+
+#print footer
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+
+print_header
+print(student_arr)
+print_footer(student_arr)
