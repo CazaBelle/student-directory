4c199732a773f2fe6758ec3c6bed9a9edb4b63d4
Carol Gardiner <carolan.gardiner@gmail.com>
2018-11-20 09:59:30 +0000

    Added data into hashes with named keys

--- directory.rb
+++ directory.rb
@@ -1,21 +1,30 @@
 
 
 
-student_arr = [ "Dr. Hannibal Lecter", "Darth Vader", "NUrse Ratched",
-  "Michael Corleone", "Alex DeLarge", "The Wicked Witch of the West",
-  "Terminator", "Freddy Krueger", "The Joker", "Joffrey Baratheon",
-  "Norman Bates" ]
+student_arr = [
+  {"name" => "Dr. Hannibal Lecter", "cohort" => :november},
+  {"name" => "Darth Vader", "cohort" => :november},
+  {"name" => "Nurse Ratched", "cohort" => :november},
+  {"name" => "Michael Corleone", "cohort" => :november},
+  {"name" => "Alex DeLarge", "cohort" => :november},
+  {"name" => "The Wicked Witch of the West", "cohort" => :november},
+  {"name" => "Terminator", "cohort" => :november},
+  {"name" => "Freddy Krueger", "cohort" => :november},
+  {"name" => "The Joker", "cohort" => :november},
+  {"name" => "Joffrey Baratheon", "cohort" => :november},
+  {"name" => "Norman Bates", "cohort" => :november}
+]
 
 
 # print header
 def print_header
-  puts "The Students of Villa&&&ins Academy"
+  puts "The Students of Villains Academy"
   puts "-------------"
 end
 
 #print student list
 def print(names)
-  names.each {|student| puts student }
+  names.each {|student| puts "#{student["name"]}, (#{student["cohort"]} cohort)" }
 end
 
 #print footer
