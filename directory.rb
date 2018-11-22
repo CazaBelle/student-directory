97f5cd065b29bb77c78458fd7b4657043f252c78
Carol Gardiner <carolan.gardiner@gmail.com>
2018-11-22 12:33:18 +0000

    Added method that saves file to csv

--- directory.rb
+++ directory.rb
@@ -1,5 +1,42 @@
 #global array that all methods can access
 @student_arr = []
+
+def interactive_menu
+  loop do 
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def print_menu 
+  # 1. print the menu and ask the user what to do
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "3. Save the list to students.csv"
+  puts "9. Exit"
+end
+
+def process(selection)
+  case selection 
+  when "1" # input the student
+    student_arr = input_students
+  when "2"
+    show_students
+  when "3"
+    save_students
+  when "9"
+    exit
+  else
+    puts "I don't know what you meant, try again"
+  end
+end
+
+def show_students
+  print_header
+  print_students_list(@student_arr)
+  print_footer(@student_arr)
+end
+
 # ask user for input
 def input_students
   puts "Please enter a name of a student"
@@ -10,11 +47,10 @@ def input_students
     if cohort.empty?
       cohort = "november"
     end
-
     add_student(name, cohort) 
     puts "Now we have #{@student_arr.count} students"
     name = gets.chomp #.tr() is the alternative to .chomp
-    if name == "quit"|| cohort == "quit"
+    if name == "9"|| cohort == "9"
       break
     end
   end
@@ -37,7 +73,7 @@ def print_header
 end
 
 #print student list
-def print(names)
+def print_students_list(names)
   cohort_group = names.group_by { |student| student[:cohort] }
   cohort_group.each do |cohort, pairs|
     puts "#{cohort.upcase}".center(100)
@@ -59,7 +95,19 @@ def print_footer(names)
   end
 end
 
-@student_arr = input_students
-print_header
-print(@student_arr)
-print_footer(@student_arr)
+def save_student
+  # opens file 
+  file = File.open("students.csv", "w")
+  # iterate over array of students
+  @student_arr.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+# @student_arr = input_students
+# print_header
+# print(@student_arr)
+# print_footer(@student_arr)
+interactive_menu
