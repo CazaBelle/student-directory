6d0f6bfb9ab45f75e1a3780fa30fce92658393fe
Carol Gardiner <carolan.gardiner@gmail.com>
2018-11-24 11:06:40 +0000

    Added csv foreach loop to allow loading and csv open to allow saving

--- directory.rb
+++ directory.rb
@@ -2,7 +2,7 @@ require "csv"
 #global array that all methods can access
 @students = []
 
-def print_menu 
+def print_menu
   # 1. print the menu and ask the user what to do
   puts "1. Input the students"
   puts "2. Show the students"
@@ -12,27 +12,35 @@ def print_menu
 end
 
 def interactive_menu
-  loop do 
+  loop do
     print_menu
     process(STDIN.gets.chomp)
   end
 end
 
 def process(selection)
-  case selection 
+  case selection
   when "1" # input the student
     students = input_students
-  when "2"
-    show_students
-  when "3"
-    save_students
-  when "4"
-    load_students
-  when "9"
-    exit
+  when "2" then show_students
+  when "3" then save_students(file)
+  when "4" then load_students(file)
+  when "9" then exit
   else
     puts "I don't know what you meant, try again"
   end
+end
+
+#specifies which file to load/save the student lists from 
+def file 
+  puts "Which file would you like to use? Hit enter to use the default students.csv"
+  file_given = STDIN.gets.chomp
+  if file_given == "" 
+    puts "Default file used"
+    "students.csv"
+    else
+      file_given
+    end
 end
 
 # ask user for input
@@ -45,7 +53,7 @@ def input_students
     if cohort.empty?
       cohort = "november"
     end
-    add_student(name, cohort) 
+    add_student(name, cohort)
     puts "Now we have #{@students.count} students"
     name = STDIN.gets.chomp #.tr() is the alternative to .chomp
     if name == "9"|| cohort == "9"
@@ -56,18 +64,18 @@ def input_students
 end
 
 def show_students
-  print_header
+  print_header unless @students.empty?
   print_students_list(@students)
   print_footer(@students)
 end
 
 # print header
 def print_header
-  if @students.length == 0 # won't print list if no names entered
-    input_students
-  elsif @students.length > 0
+  if @students.length > 0
     puts "The Students of Makers Academy".center(100)
     puts "------------------------------".center(100)
+  else
+    puts "There are no students registered"
   end
 end
 
@@ -85,8 +93,8 @@ end
 
 # Adds student and cohort to the array
 def add_student(name,cohort)
-  @students << {name: name, cohort: cohort.to_sym}
-end 
+  @students << {name: name, cohort: cohort}
+end
 
 #print footer
 def print_footer(names)
@@ -99,43 +107,54 @@ def print_footer(names)
   end
 end
 
-def save_students
+# save_students given in tutorial
+# def save_students
+#   # open the file for writing
+#   file = File.open("students.csv", "w")
+#   # iterate over the array of students
+#   @students.each do |student|
+#     student_data = [student[:name], student[:cohort]]
+#     csv_line = student_data.join(",")
+#     file.puts csv_line
+#   end
+#   file.close
+# end
+
+# save students using filename given
+def save_students(filename = "students.csv")
   # open the file for writing
-  file = File.open("students.csv", "w")
-  # iterate over the array of students
-  @students.each do |student|
-    student_data = [student[:name], student[:cohort]]
-    csv_line = student_data.join(",")
-    file.puts csv_line
+  CSV.open(filename, "w") do |csv|
+  # iterate over the array of saved students
+    @students.each do |student|
+      student_info = [student[:name], student[:cohort]]
+      csv << student_info
+    end
   end
-  file.close
+  puts "Student's information saved"
 end
 
+# Need to ask user which file to load
 def load_students (filename = "students.csv")
-  file = File.open(filename, "r")
-  file.readlines.each do |line|
-    name, cohort = line.chomp(',')
-      @students << {name: name, cohort: cohort.to_sym}
+  CSV.foreach(filename) do |line|
+    name, cohort = line
+    add_student(name, cohort)
+      # @students << {name: name, cohort: cohort.to_sym}
   end
-  file.close
+  puts "The list is loaded from students.csv"
 end
 
 def try_load_students
-  filename = ARGV.first 
-  return if filename.nil? 
+  filename = ARGV.first
+  return if filename.nil?
   if File.exist?(filename)
     load_students(filename)
     puts "Loaded #{@students.count} from #{filename}"
   else
-    puts "Sorry, #{filename} doesn't exist."
+    puts "Sorry, #{filename} doesn't exist. Loading students.csv"
+    load_students
     exit
   end
 end
-# @student_arr = input_students
-# print_header
-# print(@student_arr)
-# print_footer(@student_arr)
+
 try_load_students
 interactive_menu
-# load_students
-# # load_students(list.txt)
